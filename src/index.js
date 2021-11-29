fba82831599eb40449624d3569546ec917151db4
ElePennini <noraelesmail@gmail.com>
2021-11-29 14:56:27 +0100

    Added Search Engine

---
+++ src/index.js
@@ -0,0 +1,99 @@
+let now = new Date();
+
+let displaydate = document.querySelector("#date");
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tueday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+
+displaydate.innerHTML = `${day}, ${month} ${date} ${year}`;
+
+let displaytime = document.querySelector("#time");
+displaytime.innerHTML = `${hours}:${minutes}`;
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#maintemp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humid").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#feelslike").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#mintemp").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#maxtemp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "db5e301789f7ba7b4a0bc9f6bf6f135f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+function searchLocation(position) {
+  let apiKey = "db5e301789f7ba7b4a0bc9f6bf6f135f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function showCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocation = document.querySelector("#currentLoc");
+currentLocation.addEventListener("click", showCurrentLocation);
+
+searchCity("Athens");
